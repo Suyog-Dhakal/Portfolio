51e3ae3c9a2330ef0be12a6ef58e5fba8323581e
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2022-05-06 13:28:12 +0545

    home description added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,33 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return <div className="w-full h-screen bg-[#0a192f]"></div>;
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is </p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Suyog Dhakal
+        </h1>
+        <br />
+        <h5 className="text-3xl sm:text-6xl font-bold text-[#8892b0]">
+          I'm a Computer Engineering Undergraduate
+        </h5>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          Proactive and excited to partner with like-minded individuals to
+          achieve goals.
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
