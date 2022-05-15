73bdd74776b55a589defb8578ef6867f6c2c4532
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2022-05-15 19:54:50 +0545

    getform used

---
+++ src/components/Contact.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/5e4400ee-83be-4b99-98a8-edf3cbe427bf"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            Submit the form below or send me an email at -
+            suyogdhakal8@gmail.com
+          </p>
+        </div>
+        <input
+          className=" bg-slate-300 p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-slate-300"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+
+        <textarea
+          className="bg-slate-300 p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+
+        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+          Let's Collaborate
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
